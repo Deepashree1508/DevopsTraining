012611bd22109c168b242c6920c14d8646fb51c9
Deepashree <deepashree.m1@tcs.com>
2023-11-30 13:03:03 +0000

    created second branch to replicate merge conflict

--- src/main/java/com/edureka/sample/SampleApplication.java
+++ src/main/java/com/edureka/sample/SampleApplication.java
@@ -3,7 +3,8 @@ package com.edureka.sample;
 
 public class SampleApplication {
  public void run() {
-	 System.out.println("Started the practice"); 
+	 System.out.println("Started the practice");
+	 System.out.println("Trying to create merge issue");
  }
  public static void main(String[] args) throws Exception {
 	 SampleApplication application= new SampleApplication();
