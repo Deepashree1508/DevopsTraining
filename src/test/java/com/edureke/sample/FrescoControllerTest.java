3b6e9dc2262403e6a332483931debfe128a9d1d7
Deepashree <deepashree.m1@tcs.com>
2023-12-01 11:15:49 +0000

    changed packing

--- src/test/java/com/edureke/sample/FrescoControllerTest.java
+++ src/test/java/com/edureke/sample/FrescoControllerTest.java
@@ -1,10 +1,12 @@
 package com.edureke.sample;
 
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.annotation.Testable;
 
 import com.edureka.sample.FrescoController;
-
+@Testable
 public class FrescoControllerTest {
 
 	FrescoController fc= new FrescoController();
